451a38adba2108684bccccd7c47724931c26e593
Vojtěch Krása <vojta.krasa@gmail.com>
2020-05-04 13:37:10 +0200

    Add Bundled Plugins to IntelliJ Platform Plugin SDK

---
+++ src/krasa/usefulactions/AddBundledPluginsToSdkAction.java
@@ -0,0 +1,87 @@
+package krasa.usefulactions;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.SdkModificator;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
+
+public class AddBundledPluginsToSdkAction extends AnAction {
+	private static final Logger LOG = Logger.getInstance(AddBundledPluginsToSdkAction.class);
+
+	@NonNls
+	private static final String PLUGINS_DIR = "plugins";
+	@NonNls
+	private static final String LIB_DIR_NAME = "lib";
+	@NonNls
+	private static final String SRC_DIR_NAME = "src";
+
+	@java.lang.Override
+	public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+		Project eventProject = getEventProject(anActionEvent);
+		Sdk projectSdk1 = ProjectRootManager.getInstance(eventProject).getProjectSdk();
+		if (projectSdk1.getSdkType().getName().equals("IDEA JDK")) {
+			SdkModificator sdkModificator = projectSdk1.getSdkModificator();
+			String homePath = sdkModificator.getHomePath();
+
+			VirtualFile[] ideaLibrary = getIdeaLibrary(homePath);
+
+			for (VirtualFile aIdeaLib : ideaLibrary) {
+				sdkModificator.addRoot(aIdeaLib, OrderRootType.CLASSES);
+			}
+			sdkModificator.commitChanges();
+			LOG.info("SDK updated");
+		}
+	}
+
+	private static void appendIdeaLibrary(@NotNull String libDirPath, @NotNull List<VirtualFile> result, @NonNls final String... forbidden) {
+		Arrays.sort(forbidden);
+		final String path = libDirPath + File.separator + LIB_DIR_NAME;
+		final JarFileSystem jfs = JarFileSystem.getInstance();
+		final File lib = new File(path);
+		if (lib.isDirectory()) {
+			File[] jars = lib.listFiles();
+			if (jars != null) {
+				for (File jar : jars) {
+					@NonNls
+					String name = jar.getName();
+					if (jar.isFile() && Arrays.binarySearch(forbidden, name) < 0 && (name.endsWith(".jar") || name.endsWith(".zip"))) {
+						VirtualFile file = jfs.findFileByPath(jar.getPath() + JarFileSystem.JAR_SEPARATOR);
+						LOG.assertTrue(file != null, jar.getPath() + " not found");
+						result.add(file);
+					}
+				}
+			}
+		}
+	}
+
+	private static VirtualFile[] getIdeaLibrary(String home) {
+		List<VirtualFile> result = new ArrayList<>();
+		String plugins = home + File.separator + PLUGINS_DIR + File.separator;
+		final JarFileSystem jfs = JarFileSystem.getInstance();
+		final File lib = new File(plugins);
+		if (lib.isDirectory()) {
+			File[] dirs = lib.listFiles();
+			if (dirs != null) {
+				for (File dir : dirs) {
+					appendIdeaLibrary(plugins + dir.getName(), result);
+				}
+			}
+		}
+		return VfsUtilCore.toVirtualFileArray(result);
+	}
+}
